21604aa0699c98988310354c82842631c00bb5d1
Jens Kapitza <j.kapitza@schwarze-allianz.de>
2014-10-12 14:22:08 +0200

    update pom file add startup class

---
+++ fuzzy-avenger/modules/fuzzy-gui/src/main/java/de/back2heaven/fuzzy/gui/FXFuzzyStart.java
@@ -0,0 +1,16 @@
+package de.back2heaven.fuzzy.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FXFuzzyStart extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+}
