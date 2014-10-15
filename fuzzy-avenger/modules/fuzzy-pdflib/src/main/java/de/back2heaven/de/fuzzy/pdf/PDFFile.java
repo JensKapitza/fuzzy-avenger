837dc2d2ade18b4c415c35470ab03bbc72648670
Jens Kapitza <j.kapitza@schwarze-allianz.de>
2014-10-15 23:23:57 +0200

    move to pdfbox version 2.0
    add more libs for later use
    add simple page renderer
    detect pages with images only

--- fuzzy-avenger/modules/fuzzy-pdflib/src/main/java/de/back2heaven/de/fuzzy/pdf/PDFFile.java
+++ fuzzy-avenger/modules/fuzzy-pdflib/src/main/java/de/back2heaven/de/fuzzy/pdf/PDFFile.java
@@ -1,11 +1,15 @@
 package de.back2heaven.de.fuzzy.pdf;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
-import org.apache.pdfbox.exceptions.CryptographyException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.encryption.DecryptionMaterial;
+import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
+import org.apache.pdfbox.pdmodel.encryption.StandardDecryptionMaterial;
+import org.apache.pdfbox.rendering.PDFRenderer;
 
 public class PDFFile {
 	private Path path;
@@ -16,20 +20,36 @@ public class PDFFile {
 
 	private String password;
 
+	private PDFRenderer renderer;
+
+	public PDFFile(Path path, String password) throws IOException {
+		this.path = path;
+		this.password = password;
+		if (path == null || !Files.exists(path)) {
+			throw new IOException("pdf file not found");
+		}
+		document = PDDocument.loadNonSeq(path.toFile());
+		parse();
+		renderer = new PDFRenderer(document);
+		this.pages = document.getNumberOfPages();
+	}
+
 	private void parse() throws IOException {
 		if (document == null) {
 			throw new IOException("document not open");
 		}
 
 		if (document.isEncrypted()) {
+			if (password == null) {
+				// hackO
+				password = "";
+			}
+			DecryptionMaterial decryptionMaterial = new StandardDecryptionMaterial(
+					password);
 			try {
-				if (password == null) {
-					document.decrypt("");
-				} else {
-					document.decrypt(password);
-				}
-			} catch (CryptographyException e) {
-				throw new IOException("problems with decrypting document");
+				document.openProtection(decryptionMaterial);
+			} catch (InvalidPasswordException e) {
+				throw new IOException("password invalid for file: " + path);
 			}
 		}
 
@@ -42,8 +62,21 @@ public class PDFFile {
 		if (pageNum > document.getNumberOfPages()) {
 			throw new IOException("page index is out of range");
 		}
-		PDPage page = (PDPage) document.getDocumentCatalog().getAllPages().get(pageNum);
-		return new PDFPage(this, page, pageNum);
+		PDPage page = (PDPage) document.getDocumentCatalog().getAllPages()
+				.get(pageNum);
+		return new PDFPage(this,renderer, page, pageNum);
+	}
+
+	public int getPages() {
+		return pages;
+	}
+
+	public PDDocument getDocument() {
+		return document;
+	}
+
+	public Path getPath() {
+		return path;
 	}
 
 }
--- fuzzy-avenger/modules/fuzzy-pdflib/src/main/java/de/back2heaven/de/fuzzy/pdf/PDFPage.java
+++ fuzzy-avenger/modules/fuzzy-pdflib/src/main/java/de/back2heaven/de/fuzzy/pdf/PDFPage.java
@@ -1,16 +1,18 @@
 package de.back2heaven.de.fuzzy.pdf;
 
-import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.graphics.PDXObject;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImage;
+import org.apache.pdfbox.rendering.PDFRenderer;
 
 public class PDFPage {
 	private PDPage page;
-	private Image prev;
-	private BufferedImage printable;
 
 	private int pageNum;
 
@@ -19,17 +21,23 @@ public class PDFPage {
 	private int rotation;
 
 	private boolean containsImagesOnly;
+	private PDFRenderer renderer;
+	private PDImage innerData;
 
-	public PDFPage(PDFFile file,PDPage page, int pageNum) throws IOException {
+	private float dpi = 300;
+
+	public PDFPage(PDFFile file, PDFRenderer renderer, PDPage page, int pageNum)
+			throws IOException {
 		this.pdfFile = file;
+		this.renderer = renderer;
 		if (pdfFile == null) {
 			throw new IOException("pdf file must be added for page def.");
 		}
-		
+
 		// we need to load the page now
 		this.page = page;
 		this.pageNum = pageNum;
-		
+
 		if (page == null || pageNum < 0) {
 			throw new IOException("the given page does not exist index:"
 					+ pageNum);
@@ -41,17 +49,57 @@ public class PDFPage {
 
 	private void parse() throws IOException {
 		rotation = page.findRotation();
-		printable = page.convertToImage(BufferedImage.TYPE_INT_ARGB, 300);
-		// die prev. ist immer 200px groß
-		prev = printable.getScaledInstance(200, -1, Image.SCALE_FAST);
 
 		// nur images auf der seite?
-		// ich muss diese extrahieren wenn kein text hier steht, 
+		// ich muss diese extrahieren wenn kein text hier steht,
 		// vom kopierer gebautes PDF?
-		
-		containsImagesOnly = false;
-		// sollte das der Fall sein, dann muss ich die prev. nicht berechnen, kann das bild einfach nutzen.
-		
-	
+		Map<String, PDXObject> allObj = page.getResources().getXObjects();
+
+		List<PDXObject> liste = allObj.entrySet().stream()
+				.map(e -> e.getValue()).collect(Collectors.toList());
+		List<PDImage> images = liste.stream().filter(p -> p instanceof PDImage)
+				.map(e -> (PDImage) e).collect(Collectors.toList());
+
+		containsImagesOnly = liste.size() == images.size();
+
+		if (containsImagesOnly && images.size() == 1) {
+			innerData = images.get(0);
+		}
+
 	}
+
+	public BufferedImage getImage() throws IOException {
+		if (innerData != null) {
+			return innerData.getImage();
+		}
+		return renderer.renderImageWithDPI(pageNum, dpi);
+	}
+
+	public int getRotation() {
+		return rotation;
+	}
+
+	public PDFFile getParent() {
+		return pdfFile;
+	}
+
+	public PDPage getPage() {
+		if (innerData != null) {
+			return page;
+		}
+		return null;
+	}
+
+	public boolean containsImagesOnly() {
+		return containsImagesOnly;
+	}
+
+	public void setDPI(float dpi) {
+		this.dpi = dpi;
+	}
+
+	public float getDPI() {
+		return dpi;
+	}
+
 }
